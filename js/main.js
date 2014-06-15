839f63740ed5a28fcc9e78866f4df452b183f9f7
Jonathan White <jonathanwhite.dev@gmail.com>
2014-06-15 12:29:00 -0700

    Added dynamic card generation

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ $(document).ready(function() {
         xCoordinateEnd: 0,
         yCoordinateStart: 0,
         xCoordinateEnd: 0,
+        cardIndex: 0,
 
         initializer: function () {
             var self = this;
@@ -42,7 +43,7 @@ $(document).ready(function() {
             var self = this;
             var $card = $('.card');
 
-            $card.on('touchstart', function (event) {
+            $(document.body).on('touchstart', $card, function (event) {
                 self.touchStart(event, this);
             }).on('touchend', function (e) {
                 self.touchEnd(event, this);
@@ -61,20 +62,78 @@ $(document).ready(function() {
         touchEnd: function (e, context) {
             var self = this,
                 userIntent,
-                touch = e.changedTouches[0];
+                touch = e.changedTouches[0],
+                $lastCard = $('.card').last(),
+                $newCard = $('.card').first(),
+                cardTemplate = '<section class="card"><img class="cardProfile" src=""><header class="overflowAuto"><div class="cardHeaderContainer"><h1 class="cardAuthor"></h1><p class="cardLocation"></p></div></header><div class="cardEventContainer"><h1 class="cardTitle" id="cardTitle"></h1><p class="cardCopy" id="description"></p><h1 class="cardTitle">When</h1><p class="cardCopy" id="when"></p><h1 class="cardTitle">Where</h1><p class="cardCopy" id="where">Swipe right to find out!</p><h1 class="cardTitle">Going</h1><p class="cardCopy" id="going"></p></div></section>',
+                mockData = {
+                    "card_list": [
+                        {
+                            "img": "img/profile1.png",
+                            "author": "Andrea",
+                            "location": "Santa Monica",
+                            "title": "Example 1",
+                            "description": "test1",
+                            "when": "test1",
+                            "going": "test1"
+                        },
+                        {
+                            "img": "img/profile1.png",
+                            "author": "Andrea",
+                            "location": "Orange County",
+                            "title": "Example 2",
+                            "description": "test2",
+                            "when": "test2",
+                            "going": "test2"
+                        },
+                        {
+                            "img": "img/profile1.png",
+                            "author": "Andrea",
+                            "location": "Orange County",
+                            "title": "Example 3",
+                            "description": "test3",
+                            "when": "test3",
+                            "going": "test3"
+                        },
+                        {
+                            "img": "img/profile1.png",
+                            "author": "Andrea",
+                            "location": "Orange County",
+                            "title": "Example 4",
+                            "description": "test4",
+                            "when": "test4",
+                            "going": "test4"
+                        }
+                    ]    
+                };
 
             card.xCoordinateEnd = touch.pageX;
             card.yCoordinateEnd = touch.pageY;
 
             userIntent = self.computeIntent();
 
+            // Prepends new card template
+            $('.card').before(cardTemplate);
+            // Sets values of card template
+            $('.cardProfile').first().attr('src', mockData.card_list[self.cardIndex].img);
+            $('.cardAuthor').first().html(mockData.card_list[self.cardIndex].author);
+            $('.cardLocation').first().html('in ' + mockData.card_list[self.cardIndex].location);
+            $('#cardTitle').first().html(mockData.card_list[self.cardIndex].title);
+            $('#description').first().html(mockData.card_list[self.cardIndex].description);
+            $('#when').first().html(mockData.card_list[self.cardIndex].when);
+            $('#going').first().html(mockData.card_list[self.cardIndex].going);
+
             if (userIntent === 'left') {
-                $(context).addClass('leaveLeft');
+                $lastCard.addClass('leaveLeft');
             } else {
-                $(context).addClass('leaveRight');
+                $lastCard.addClass('leaveRight');
             }
 
-            $(context).fadeOut(400);
+            $('.card').last().fadeOut(400, function () {
+                $lastCard.remove();
+            });
+
+            self.cardIndex++;
         },
 
         computeIntent: function() {
